a65504f3ac87f9e25383031872d96118a46b5b43
Pavel Apasov <apasovp@seznam.cz>
2023-07-14 23:13:19 +0200

    Add(mitochjndrion,lysosome,nucleo) + styles

--- WinFormsApp1/Interface/app.js
+++ WinFormsApp1/Interface/app.js
@@ -3,10 +3,94 @@
 var app = new Vue({
     el: "#app",
     data: {
-        coordinates: {
-            left: "100px",
-            top: "100px"
-        },
+
+        cells: [
+            {
+                life: 100,
+                style: {
+                    width: "150px",
+                    height: "150px",
+                    left: "100px",
+                    top: "100px"
+
+                },
+                mitochondrions: [
+                    {
+
+                        style: {
+                            left: "100px",
+                            top: "100px",
+                            transform: "rotate(45deg)"
+                        },
+                    },
+                    {
+                        style: {
+                            left: "10px",
+                            top: "30px",
+                            transform: "rotate(110deg)"
+
+
+                        },
+                    },
+                    {
+
+                        style: {
+                            left: "30px",
+                            top: "70px",
+                            transform: "rotate(-10deg)"
+
+
+                        },
+                    
+                    },
+                ]
+            },
+
+            {
+                life: 100,
+                style: {
+                    width: "100px",
+                    height: "100px",
+                    left: "500px",
+                    top: "300px"
+
+                },
+
+                mitochondrions: [
+                    {
+
+                        style: {
+                            left: "100px",
+                            top: "100px",
+                            transform: "rotate(45deg)"
+
+
+                        },
+                    },
+                    {
+
+                        style: {
+                            left: "10px",
+                            top: "30px",
+                            transform: "rotate(110deg)"
+
+
+                        },
+                    },
+                    {
+
+                    
+                        style: {
+                            left: "30px",
+                            top: "70px",
+                            transform: "rotate(-10deg)"
+
+
+                        },
+                    },
+                ]
+            },
+        ],
         form: null
 
     },
@@ -17,14 +101,14 @@ var app = new Vue({
         ShowCellInfo() {
             api.GetPosition();
 
-              this.form.show();
+            /*  this.form.show();*/
         },
         HideCellInfo() {
             this.form.hide();
 
         },
         SetPosition(point) {
-            this.coordinates = point;
+            this.style = point;
         }
 
     }
